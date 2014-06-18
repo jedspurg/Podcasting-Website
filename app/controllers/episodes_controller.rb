9dd8a1869afb91e74d79ca08cee6c4fe679289bb
Jeramy Spurgeon <jeramy.d.spurgeon@gmail.com>
2014-06-18 19:54:04 -0400

    Log in and permissions

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,5 @@
 class EpisodesController < ApplicationController
+  load_and_authorize_resource
 
   def index
     @episodes = Episode.all
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,9 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.admin?
+  def initialize(current_user)
+    @user = current_user || User.new # guest user (not logged in)
+    if @user.admin?
       can :manage, :all
     else
       can :read, :all
